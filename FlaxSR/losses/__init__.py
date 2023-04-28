98d71d6a7c58bb546c3f2f32bd5682bd65facf1e
dslisleedh <dslisleedh@gmail.com>
2023-04-28 14:13:20 +0900

    change perceptual_loss.py to perceptual_losses.py

--- FlaxSR/losses/__init__.py
+++ FlaxSR/losses/__init__.py
@@ -3,7 +3,7 @@ from .pixel_wise_losses import (
     l2_loss as l2_loss,
     charbonnier_loss as charbonnier_loss,
 )
-from .perceptual_loss import (
+from .perceptual_losses import (
     vgg_loss as vgg_loss,
     check_vgg_params_exists as check_vgg_params_exists,
 )
