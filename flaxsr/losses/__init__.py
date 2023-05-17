3ad59a54dd2a491599f6c9213ad676ea4398bb97
dslisleedh <gnova6kud@gmail.com>
2023-05-17 13:12:43 +0900

    fix adversarial losses to be imported

--- flaxsr/losses/__init__.py
+++ flaxsr/losses/__init__.py
@@ -6,6 +6,14 @@ from .pixel_wise_losses import (
 from .perceptual_losses import (
     vgg_loss as vgg_loss,
 )
+from .adversarial_losses import (
+    minmax_discriminator_loss as minmax_discriminator_loss,
+    minmax_generator_loss as minmax_generator_loss,
+    ls_discriminator_loss as ls_discriminator_loss,
+    ls_generator_loss as ls_generator_loss,
+    relativistic_discriminator_loss as relativistic_discriminator_loss,
+    relativistic_generator_loss as relativistic_generator_loss,
+)
 from .utils import (
     check_vgg_params_exists as check_vgg_params_exists,
     load_vgg19_params as load_vgg19_params,
