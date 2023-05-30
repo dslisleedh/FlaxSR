20d916da9d608b86fea3a3088dbd70e5a9b07803
dslisleedh <gnova6kud@gmail.com>
2023-05-31 01:14:07 +0900

    version 0.0.3a

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="flaxsr",
-    version="0.0.3",
+    version="0.0.3a",
     author="dslisleedh",
     author_email="dslisleedh@gmail.com",
     description="Super Resolution models with Jax/Flax",
@@ -19,6 +19,14 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Image Processing"
+    ],
+    install_requires=[
+        "jax",
+        "jaxlib",
+        "flax",
+        "einops",
+        "tensorflow",
+        "numpy"
     ],
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
